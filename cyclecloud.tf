ce7e23f94b53bb53a19bc29dac9801774b87e9ef
Alexandre Jean <aljean@microsoft.com>
2022-03-31 10:01:11 +0000

    update

--- cyclecloud.tf
+++ cyclecloud.tf
@@ -8,7 +8,7 @@ resource "azurerm_network_interface" "cyclecloud" {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.admin.id
     private_ip_address_allocation = "Dynamic"
-    #public_ip_address_id = azurerm_public_ip.cycle.id
+    # public_ip_address_id = azurerm_public_ip.cycle.id
   }
 
   tags = var.tags
